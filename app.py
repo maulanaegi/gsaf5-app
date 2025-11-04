a67d09480f5078fab5cc811d4bb91c488af4452e
egirangga <malikazxas@gmail.com>
2025-11-04 15:30:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 # === URL dataset di GitHub ===
-DATA_URL = "DATA_URL = "https://raw.githubusercontent.com/maulanaegi/gsaf5-app/main/GSAF5.xlsx
-"
+DATA_URL = "DATA_URL = "https://raw.githubusercontent.com/maulanaegi/gsaf5-app/main/GSAF5.xlsx"
 "
 # Ganti <username>, <repo>, dan nama file sesuai repo kamu
 
@@ -133,5 +132,6 @@ else:
 
 st.markdown("---")
 st.caption("Dibuat dengan ❤️ menggunakan Streamlit & Plotly | © 2025")
+
 
 
